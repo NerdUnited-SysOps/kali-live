a8bac2dca5417ae48cd63f27fc79c1db0c01d280
Jason Leckie <jasonleckie@Jasons-MacBook-Pro.local>
2023-02-10 14:20:50 -0700

    Jan and Feb changes for ISO

---
+++ kali-config/common/includes.chroot/home/user/clean.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/zsh
+
+mv -v ~/.config/xfce4/panel/launcher-6/blockexplorer.desktop ~/.config/xfce4/panel/launcher-6/blockexplorer.desktop_
+rm -rv ~/blockfabric-ceremony/
+rm -rv ~/.aws/
+rm -v ~/*_started
+
