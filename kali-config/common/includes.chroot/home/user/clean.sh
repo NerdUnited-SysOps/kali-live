1e038c2c03a129a74b58fbc03ae2bef71ba6a3b7
jasonleckienu <113492785+jasonleckienu@users.noreply.github.com>
2023-02-13 15:46:56 -0700

    Update clean.sh

--- kali-config/common/includes.chroot/home/user/clean.sh
+++ kali-config/common/includes.chroot/home/user/clean.sh
@@ -3,5 +3,5 @@
 mv -v ~/.config/xfce4/panel/launcher-6/blockexplorer.desktop ~/.config/xfce4/panel/launcher-6/blockexplorer.desktop_
 rm -rv ~/blockfabric-ceremony/
 rm -rv ~/.aws/
-rm -v ~/*_started
+touch ~/system_started
 
