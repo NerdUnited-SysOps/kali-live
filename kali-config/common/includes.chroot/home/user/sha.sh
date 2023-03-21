5f186fdc6502c3ba1e9a83ac0ea862f4e1904ec0
jasonleckienu <113492785+jasonleckienu@users.noreply.github.com>
2023-03-21 01:02:45 +0000

    sha.sh for sha256sum operations

---
+++ kali-config/common/includes.chroot/home/user/sha.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/zsh
+
+if (( $# < 1 )); then
+    echo; echo "Exiting. Expected  volumeX"; echo
+    exit 1
+elif (( $# > 1 )); then
+    echo; echo "Too many arguments.  Only volumeX is expected."; echo
+    exit 2
+fi
+
+
+find $1/ -type f -exec sha256sum "{}" + | sudo tee /media/usb/ceremony/$1.laptop.sha
